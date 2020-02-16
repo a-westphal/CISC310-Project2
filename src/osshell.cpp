63eb43e663046f35e7ebf071a81b0c2eb20a8f2d
a-westphal <west8659@stthomas.edu>
2020-02-16 16:06:00 -0600

    issues with file transfer

--- src/osshell.cpp
+++ src/osshell.cpp
@@ -12,18 +12,42 @@ int main (int argc, char **argv)
     std::string input;
     char* os_path = getenv("PATH");
     std::vector<std::string> os_path_list = splitString(os_path, ':');
+    bool done = false;
 
     std::cout << "Welcome to OSShell! Please enter your commands ('exit' to quit)." << std::endl;
+    while(!done)
+    {
+        std::cout <<"osshell> ";
+        std::getline (std::cin,input);
+    
+        //if the input includes "history": 
+        if(input.find("history") >= 0 && input.find("history") <= input.length())
+        {
+            //the string history is here! 
+            std::cout << "The word history is in your input!" << std::endl;
+            for(int i = 0; i < input.length(); i ++)
+            {
+                
+            }
+        }
+        // Repeat:
+        //  Print prompt for user input: "osshell> " (no newline)
+        //  Get user input for next command
+        //  If command is `exit` exit loop / quit program
+        //  If command is `history` print previous N commands
+        //  For all other commands, check if an executable by that name is in one of the PATH directories
+        //  If yes, execute it
+        //  If no, print error statement: "<command_name>: Error running command" (do include newline)
 
-    // Repeat:
-    //  Print prompt for user input: "osshell> " (no newline)
-    //  Get user input for next command
-    //  If command is `exit` exit loop / quit program
-    //  If command is `history` print previous N commands
-    //  For all other commands, check if an executable by that name is in one of the PATH directories
-    //   If yes, execute it
-    //   If no, print error statement: "<command_name>: Error running command" (do include newline)
+        //if the input is "exit": 
+        if(input.find("exit") >= 0 && input.find("exit") <= input.length())
+        {
+            //end the program and loop
+            done = true;
+        }
+    }
 
+    return 0;
     return 0;
 }
 
